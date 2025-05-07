208a564f63e69daf53dacc6591af05c1ac8fb3d4
FigoFavian <figo21fr@gmail.com>
2025-05-07 13:20:05 +0700

    Transaction Service Implementation

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
+        .build_client(true)
+        // adjust to your .proto location:
         .compile(
-            &["proto/services.proto"], 
+            &["proto/services.proto"],
             &["proto"],
-    // Directory where the proto file is located
         )?;
     Ok(())
 }
